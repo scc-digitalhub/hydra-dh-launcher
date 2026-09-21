c93b7a707f5e7dd89c76546119ffec01a9f83741
kazhamiakin <kazhamiakin@gmail.com>
2026-09-21 14:48:01 +0200

    Revert to multithread version

--- hydra_plugins/hydra_dh_launcher/_core.py
+++ hydra_plugins/hydra_dh_launcher/_core.py
@@ -5,21 +5,19 @@
 import logging
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
-import time
 from typing import Any, Dict, List, Sequence
 import re
 
+from hydra.core.hydra_config import HydraConfig
 from hydra.core.singleton import Singleton
 from hydra.core.utils import (
     JobReturn,
-    JobStatus,
     configure_log,
     filter_overrides,
     run_job,
     setup_globals,
 )
-from hydra.types import HydraContext
-
+from hydra.types import HydraContext, TaskFunction
 from omegaconf import DictConfig, OmegaConf, open_dict
 
 from .dh_launcher import DHLauncher
@@ -61,10 +59,8 @@ def execute_job(
     job_dir = os.path.abspath(output_dir)
 
     def task_function(cfg: DictConfig) -> Any:
-        local_execution = func_config.get("local_execution", False)
-
         run = func.run(action="subtask", 
-            wait=local_execution, 
+            wait=True, 
             log_info=False,
             volumes=func_config.get("volumes", None),
             resources=func_config.get("resources", None),
@@ -78,20 +74,17 @@ def execute_job(
                 "job_dir": job_dir,
                 },
             job_ref=func_config.get("job_ref", None),
-            local_execution=local_execution,
+            local_execution=func_config.get("local_execution", False),
         )
         status = run.refresh().status
         if status.state == "ERROR":
             raise RuntimeError(f"Job failed with error: {status.message or 'Execution error'}")
 
-        if local_execution:
-            values = [v for k, v in status.results.items()]
-            if len(status.results) == 1:
-                return values[0]
-            else:   
-                return values
-        else:
-            return run.id
+        values = [v for k, v in status.results.items()]
+        if len(status.results) == 1:
+            return values[0]
+        else:   
+            return values
 
     ret = run_job(
         hydra_context=hydra_context,
@@ -119,78 +112,6 @@ def _get_function_signature(function: str) -> tuple[str, str]:
         # function is in format <function_name>, use default version
         return function, None
 
-def run_and_monitor_remote_jobs(
-        initial_job_idx: int,
-        job_overrides: Sequence[Sequence[str]],
-        hydra_context: HydraContext,
-        config: DictConfig,
-        func_config: DictConfig,
-        project_name: str,
-        func,
-        singleton_state: Dict[Any, Any],
-) -> Sequence[JobReturn]:
-    """Run and monitor remote jobs.
-
-    This function executes a series of remote jobs based on the provided job overrides and monitors their status until completion.
-    If any job fails, a RuntimeError is raised.
-    The function returns the results of all successfully completed jobs.
-
-    Args:
-        initial_job_idx (int): _description_
-        job_overrides (Sequence[Sequence[str]]): _description_
-        hydra_context (HydraContext): _description_
-        config (DictConfig): _description_
-        func_config (DictConfig): _description_
-        project_name (str): _description_
-        func (_type_): _description_
-        singleton_state (Dict[Any, Any]): _description_
-
-    Raises:
-        RuntimeError: _description_
-
-    Returns:
-        Sequence[JobReturn]: _description_
-    """
-    runs = []
-    for idx, overrides in enumerate(job_overrides):
-        job_return = execute_job(
-            initial_job_idx + idx,
-            overrides,
-            hydra_context,
-            config,
-            func_config,
-            func,
-            singleton_state,
-        )
-        if job_return.status != JobStatus.COMPLETED:
-            raise RuntimeError(f"Job failed with status: {job_return.status}")
-        runs.append(job_return)
-
-    to_complete = len(runs)
-    results = [None] * len(runs)
-    project = dh.get_project(project_name)
-    log.info(f"Monitoring {len(runs)} remote jobs.")
-
-    while to_complete > 0:    
-        for idx, job_return in enumerate(runs):
-            # result has not been retrieved yet
-            if results[idx] is None:
-                run_id = job_return.return_value
-                run = project.get_run(run_id)
-                status = run.refresh().status
-                if status.state == "ERROR":
-                    raise RuntimeError(f"Job failed with error: {status.message or 'Execution error'}")
-                if status.state == "COMPLETED":
-                    values = [v for k, v in status.results.items()]
-                    if len(status.results) == 1:
-                        values = values[0]
-                    job_return.return_value = values
-                    results[idx] = job_return
-                    to_complete -= 1
-        time.sleep(10)
-        log.info(f"{to_complete} jobs remaining.")
-    return results
-
 def launch(
     launcher: DHLauncher,
     job_overrides: Sequence[Sequence[str]],
@@ -215,7 +136,6 @@ def launch(
 
 
     dhlauncher_config = launcher.dh
-    ## check if the function exists in the project
     ## recover function. 
     ## if exists, check the source code signature to make sure it is the same. Otherwise, create new version.
     project_name = dhlauncher_config['project_name']
@@ -233,7 +153,7 @@ def launch(
         func = dh.get_function(function_name, project=project_name, entity_id=version)
         # enforce task 
         func._get_or_create_task("subtask")
-    except Exception:
+    except Exception as e:
         raise ValueError(f"Function {function} does not exist in project {project_name}.")
     
     n_jobs = dhlauncher_config.get("n_jobs", -1)
@@ -251,41 +171,25 @@ def launch(
 
     singleton_state = Singleton.get_state()
 
+    futures_map = {}
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for idx, overrides in enumerate(job_overrides):
+            future = executor.submit(
+                execute_job,
+                initial_job_idx + idx,
+                overrides,
+                launcher.hydra_context,
+                launcher.config,
+                dhlauncher_config,
+                func,
+                singleton_state,
+            )
+            futures_map[future] = initial_job_idx + idx
+
     runs: List[JobReturn] = [None] * len(job_overrides)  # type: ignore
-
-    local_execution = dhlauncher_config.get("local_execution", False)
-    # Execute jobs locally with thread pool executor and blocking
-    if local_execution:
-        futures_map = {}
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            for idx, overrides in enumerate(job_overrides):
-                future = executor.submit(
-                    execute_job,
-                    initial_job_idx + idx,
-                    overrides,
-                    launcher.hydra_context,
-                    launcher.config,
-                    dhlauncher_config,
-                    func,
-                    singleton_state,
-                )
-                futures_map[future] = initial_job_idx + idx
-
-        for future in as_completed(futures_map):
-            idx = futures_map[future] - initial_job_idx
-            runs[idx] = future.result()
-    else:
-        runs = run_and_monitor_remote_jobs(
-            initial_job_idx,
-            job_overrides,
-            launcher.hydra_context,
-            launcher.config,
-            dhlauncher_config,
-            project_name,
-            func,
-            singleton_state,
-        )
-
+    for future in as_completed(futures_map):
+        idx = futures_map[future] - initial_job_idx
+        runs[idx] = future.result()
 
     for run in runs:
         assert isinstance(run, JobReturn)
